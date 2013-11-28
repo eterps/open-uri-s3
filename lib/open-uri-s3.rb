45153da40fe2459c319a0b75b82ff6a1fb70ad75
Erik Terpstra <erterpstra@gmail.com>
2013-11-28 14:30:21 +0100

    Add implementation

---
+++ lib/open-uri-s3.rb
@@ -0,0 +1,23 @@
+module URI
+  class S3 < Generic
+    def open(*args)
+      s3 = AWS::S3.new
+      bucket = s3.buckets[self.hostname]
+      if bucket.location_constraint
+        s3 = AWS::S3.new(s3_endpoint: "s3-#{bucket.location_constraint}.amazonaws.com")
+        bucket = s3.buckets[self.hostname]
+      end
+
+      path = self.path[1..-1]
+      object = bucket.objects[path]
+
+      if args[0] == 'w'
+        yield object
+      end
+
+      object
+    end
+  end
+
+  @@schemes['S3'] = S3
+end
