e085928226a9cd9eac6e097d3990334f8b80a952
Beth <beth@bethesque.com>
2015-03-06 13:54:14 +1100

    Fully qualify AWS module.

--- lib/open-uri-s3.rb
+++ lib/open-uri-s3.rb
@@ -6,10 +6,10 @@ module URI
   class S3 < Generic
     # @return [AWS::S3::S3Object] S3 object (quacks like IO)
     def open(*args)
-      s3 = AWS::S3.new
+      s3 = ::AWS::S3.new
       bucket = s3.buckets[self.hostname]
       if bucket.location_constraint
-        s3 = AWS::S3.new(s3_endpoint: "s3-#{bucket.location_constraint}.amazonaws.com")
+        s3 = ::AWS::S3.new(s3_endpoint: "s3-#{bucket.location_constraint}.amazonaws.com")
         bucket = s3.buckets[self.hostname]
       end
 
